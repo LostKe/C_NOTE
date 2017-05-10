218a9c6060358235b34bbc71a5888c4ff119c57b
root <shuqing.me@gmail.com>
2017-05-10 10:27:26 +0800

    up

--- code/test2.c
+++ code/test2.c
@@ -1,12 +1,13 @@
 #include "stdio.h"
-
+#define GET_ARRAY_LEN(array,len){len=(sizeof(array)/sizeof(array[0]));}
 /*求数组的平均值 (形式参数是一个指针)*/
-void getAvg(int *result,int *array)
+void getAvg(int *result,int *array,int length)
 {
-  result=0;
-  for(int i;i<sizeof(array);i++)
+  *result=0;
+  int i=0;
+  for(i;i<length;i++)
   {
-    result=result+array[i];
+   *result=*result+array[i];
   }
 
 }
@@ -15,6 +16,8 @@ int main()
 {
   int array[]={1,2,3,4,5,6,7,8,9};
   int result;
-  getAvg(&result,array);
+  int len;
+  GET_ARRAY_LEN(array,len);
+  getAvg(&result,array,len);
   printf("result=%d\n",result );
 }
