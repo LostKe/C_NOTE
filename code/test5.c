b0b6995e9c8e2de2d4b31dc1f2aeac373232c354
zsq <shu_qingzhang@foxmail.com>
2017-05-10 16:19:11 +0800

    up

--- code/test5.c
+++ code/test5.c
@@ -1,5 +1,6 @@
 /*结构体*/
 #include "stdio.h"
+#include "string.h"
 
 struct Book{
    char title[50];
@@ -13,11 +14,11 @@ int main() {
   /* 设置nginx 信息*/
   strcpy(nginx.title,"NGINX 导读");
   strcpy(nginx.author,"Toms.divide");
-  nginx.book_id=002314;
+  nginx.book_id=1002314;
   /* mongoDB 信息*/
   strcpy(nginx.title,"mongoDB入门与精通");
   strcpy(nginx.author,"Toms.divide");
-  nginx.book_id=009983;
+  nginx.book_id=1009983;
 
   /*输出 nginx 信息*/
   printf("nginx-title=%s\n",nginx.title );
