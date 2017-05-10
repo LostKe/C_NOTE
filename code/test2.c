e72c135bc46852c19019e565f9204a0bcd7a1a08
zsq <shu_qingzhang@foxmail.com>
2017-05-10 10:44:10 +0800

    up

--- code/test2.c
+++ code/test2.c
@@ -1,8 +1,14 @@
 #include "stdio.h"
+/*没有提供 直接获取数组长度的函数，对于存放字符串的字符数组提供了一个strlen函数获取长度，其他类型一般定义如下宏来获取*/
 #define GET_ARRAY_LEN(array,len){len=(sizeof(array)/sizeof(array[0]));}
 /*求数组的平均值 (形式参数是一个指针)*/
 void getAvg(int *result,int *array,int length)
 {
+  /*
+  *   写 result  表示指针地址
+  *   写 *result 表示指针所访问的值
+  *    × *array[i] 是错误的 
+  */
   *result=0;
   int i=0;
   for(i;i<length;i++)
