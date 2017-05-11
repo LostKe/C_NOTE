69f9711263a48bef9c4a8df64b8c0bce49f9e142
zsq <shu_qingzhang@foxmail.com>
2017-05-11 10:05:43 +0800

    u

--- code/test7.c
+++ code/test7.c
@@ -1,11 +1,34 @@
 #include "stdio.h"
 
-int main()
+
+
+//写入文件
+void storeText(char *filename)
+{
+    FILE *fp;
+    fp=fopen(*filename,"w+");
+    fprintf(fp, "this is test for fprintf\n");
+    fputs("this is test for fputs \n",fp);
+    fclose(fp);
+}
+
+
+//读取文件
+void readText(char *filename)
 {
   FILE *fp;
-  fp=fopen("/home/test.txt","w+");
-  fprintf(fp, "this is test for fprintf\n");
-  fputs("this is test for fputs \n",fp);
-  fclose(fp);
+  char[255] buffer;
+  fp=fopen(*filename,"r");
+  fgets(buff,255,fp);
+  printf("%s\n", buffer);
+
+}
+
+int main()
+{
+  //写入文件 test
+  char[] filename="/home/test.txt";
+  //storeText(&filename);
+  readText(&filename);
   return 0;
 }
